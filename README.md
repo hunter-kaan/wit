5d987d4330f7f3961d3ead9879281f4103011fc8
HinterKaan <mr.igor.prokofev@gmail.com>
2017-03-06 15:38:28 +0500

    Init project

---
+++ README.md
@@ -0,0 +1,1 @@
+Bootstrap: http://getbootstrap.com/customize/?id=8daef24079c2e98ba2abd66c8de9a32f
---
+++ js/script.js
@@ -0,0 +1,6 @@
+/**
+ * Created by zero on 01.03.2017.
+ */
+$(function($) {
+	$('.dropdown-toggle').dropdown();
+});
